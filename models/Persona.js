7ccf643d3f4200b0b610a75e70886c75002c933b
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2024-01-22 21:06:35 +0000

    persona2

--- models/Persona.js
+++ models/Persona.js
@@ -1,12 +1,3 @@
-const Schema = mongoose.Schema;
 
-const personSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    age: Number,
-    favoriteFoods: [String]
-});
 
 //export default personSchema;
--- myApp.js
+++ myApp.js
@@ -4,6 +4,17 @@ const mongoose = require('mongoose');
 
 mongoose.connect((process.env.MONGO_URI), { useNewUrlParser: true, useUnifiedTopology: true });
 
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods: [String]
+});
+
 const Person = mongoose.model("Person", personSchema);
 
 
