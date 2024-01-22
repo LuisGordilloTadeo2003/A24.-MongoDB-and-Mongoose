b240dd3f84d75e25dc2c6f4ae6082445b63d2ee9
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2024-01-22 21:09:46 +0000

    persona3

--- myApp.js
+++ myApp.js
@@ -4,9 +4,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect((process.env.MONGO_URI), { useNewUrlParser: true, useUnifiedTopology: true });
 
-const Schema = mongoose.Schema;
-
-const personSchema = new Schema({
+const personSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true
