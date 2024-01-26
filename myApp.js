d124f38640a8ba995d9e3bb35940b1fe31f1c957
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2024-01-26 18:30:52 +0000

    2.1

--- myApp.js
+++ myApp.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 
 mongoose.connect((process.env.MONGO_URI), { useNewUrlParser: true, useUnifiedTopology: true });
 
+let Person;
+
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -13,7 +15,7 @@ const personSchema = new mongoose.Schema({
   favoriteFoods: [String]
 });
 
-const Person = mongoose.model('Person', personSchema);
+Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = function (done) {
   let LuisGordillo = new Person({
